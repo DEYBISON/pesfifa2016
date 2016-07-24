f13949f8fccb47667b843f5a4001a241425cb9bd
DEYBISON <deybisonmartinez@hotmail.com>
2016-07-24 09:35:25 -0500

    PES 2016
    SE AGREGÓ LA CARPETA ADMINISTRADOR

---
+++ administrator/components/com_joomlaupdate/restoration.php
@@ -0,0 +1,13 @@
+<?php
+defined('_AKEEBA_RESTORATION') or die('Restricted access');
+$restoration_setup = array(
+	'kickstart.security.password' => 'Ep2dngNLIMMZHPeQldlrC2GuN0p5cLgi',
+	'kickstart.tuning.max_exec_time' => '5',
+	'kickstart.tuning.run_time_bias' => '75',
+	'kickstart.tuning.min_exec_time' => '0',
+	'kickstart.procengine' => 'direct',
+	'kickstart.setup.sourcefile' => 'D:/xampp/htdocs/jdatlanta/tmp/Joomla_3.5.0-Stable-Update_Package.zip',
+	'kickstart.setup.destdir' => 'D:\xampp\htdocs\jdatlanta',
+	'kickstart.setup.restoreperms' => '0',
+	'kickstart.setup.filetype' => 'zip',
+	'kickstart.setup.dryrun' => '0');
